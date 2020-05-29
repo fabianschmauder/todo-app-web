7ad42ba2414854aaa3cb4409b127115010894e7c
JanRiefling <jan.riefling@web.de>
2020-05-29 11:50:23 +0200

    Add TodoController

--- backend/src/main/java/de/neuefische/todoapp/controller/TodoController.java
+++ backend/src/main/java/de/neuefische/todoapp/controller/TodoController.java
@@ -1,4 +1,14 @@
-package de.neuefische.todoapp;
+package de.neuefische.todoapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
 public class TodoController {
+
+    @GetMapping("todo")
+    public
+
 }
