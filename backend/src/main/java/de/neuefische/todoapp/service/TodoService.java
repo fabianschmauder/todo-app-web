2b51763840950550f0fb7d276187a39d25856eca
JanRiefling <jan.riefling@web.de>
2020-05-29 12:42:45 +0200

    Add get method

--- backend/src/main/java/de/neuefische/todoapp/service/TodoService.java
+++ backend/src/main/java/de/neuefische/todoapp/service/TodoService.java
@@ -1,4 +1,26 @@
 package de.neuefische.todoapp.service;
 
+import de.neuefische.todoapp.db.TodoDB;
+import de.neuefische.todoapp.model.Status;
+import de.neuefische.todoapp.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TodoService {
+
+    private TodoDB todoDB;
+
+    @Autowired
+    public TodoService(TodoDB todoDB){
+        this.todoDB = todoDB;
+    }
+
+    public ArrayList<Task> getTasks(){
+        return todoDB.getTasks();
+    }
+
 }
