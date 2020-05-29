925531aba8ac6c20283ef6c0706788a5d549862d
JanRiefling <jan.riefling@web.de>
2020-05-29 12:45:52 +0200

    Add TodoControllerTest

--- backend/src/test/java/de/neuefische/todoapp/controller/TodoControllerTest.java
+++ backend/src/test/java/de/neuefische/todoapp/controller/TodoControllerTest.java
@@ -1,5 +1,20 @@
+package de.neuefische.todoapp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoControllerTest {
 
+    @LocalServerPort
+    private int port;
+
+
+
+    @Test
+    void getAllTodo() {
+    }
 }
