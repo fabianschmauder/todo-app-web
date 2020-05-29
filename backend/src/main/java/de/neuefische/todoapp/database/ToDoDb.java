786d1f4f2e4229cd64e7b5afd74b51d895d8bdd5
Franziska Georg <franzi.georg@web.de>
2020-05-29 12:29:50 +0200

    Create sample todos in database

--- backend/src/main/java/de/neuefische/todoapp/database/ToDoDb.java
+++ backend/src/main/java/de/neuefische/todoapp/database/ToDoDb.java
@@ -1,6 +1,7 @@
 package de.neuefische.todoapp.database;
 
 import de.neuefische.todoapp.model.ToDo;
+import de.neuefische.todoapp.model.ToDoStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,7 +10,10 @@ import java.util.List;
 @Repository
 public class ToDoDb {
 
-    private final List<ToDo> toDos = new ArrayList<>();
+    private final List<ToDo> toDos = new ArrayList<>(List.of(
+            new ToDo("1", "FirstToDo", ToDoStatus.OPEN),
+            new ToDo("2", "SecondToDo", ToDoStatus.DONE)
+    ));
 
     public List<ToDo> getToDos() {
         return toDos;
