35d3ea26dfca97647709ff9449c0442b2588b0e2
Franziska Georg <franzi.georg@web.de>
2020-05-29 12:18:01 +0200

    Further enhancements in ToDoController and ToDoService

--- backend/src/main/java/de/neuefische/todoapp/controller/ToDoController.java
+++ backend/src/main/java/de/neuefische/todoapp/controller/ToDoController.java
@@ -1,9 +1,13 @@
 package de.neuefische.todoapp.controller;
 
+import de.neuefische.todoapp.model.ToDo;
 import de.neuefische.todoapp.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/todo")
@@ -14,6 +18,11 @@ private final ToDoService toDoService;
 @Autowired
     public ToDoController(ToDoService toDoService) {
     this.toDoService = toDoService;
+}
+
+@GetMapping
+public List<ToDo> getToDos() {
+    return toDoService.getToDos();
 }
 
 }
--- backend/src/main/java/de/neuefische/todoapp/service/ToDoService.java
+++ backend/src/main/java/de/neuefische/todoapp/service/ToDoService.java
@@ -1,8 +1,11 @@
 package de.neuefische.todoapp.service;
 
 import de.neuefische.todoapp.database.ToDoDb;
+import de.neuefische.todoapp.model.ToDo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ToDoService {
@@ -12,6 +15,10 @@ public class ToDoService {
     @Autowired
     public ToDoService(ToDoDb toDoDb) {
         this.toDoDb = toDoDb;
+    }
+
+    public List<ToDo> getToDos() {
+        return toDoDb.getToDos();
     }
 
 }
