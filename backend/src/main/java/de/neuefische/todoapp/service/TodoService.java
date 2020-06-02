9656a4b0f6d22c1a572ba0e340b4971b447a76b3
fabianschmauder <fabianschmauder@gmail.com>
2020-06-02 10:30:33 +0200

    Add todo item endpoint

--- backend/src/main/java/de/neuefische/todoapp/service/TodoService.java
+++ backend/src/main/java/de/neuefische/todoapp/service/TodoService.java
@@ -2,6 +2,9 @@ package de.neuefische.todoapp.service;
 
 import de.neuefische.todoapp.db.TodoDb;
 import de.neuefische.todoapp.model.TodoItem;
+import de.neuefische.todoapp.model.TodoStatus;
+import de.neuefische.todoapp.model.dto.AddTodoDTO;
+import de.neuefische.todoapp.utils.IdGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +13,26 @@ import java.util.List;
 @Service
 public class TodoService {
   private final TodoDb db;
+  private final IdGenerationService idGenerationService;
 
   @Autowired
-  public TodoService(TodoDb db) {
+  public TodoService(TodoDb db, IdGenerationService idGenerationService) {
     this.db = db;
+    this.idGenerationService = idGenerationService;
   }
 
   public List<TodoItem> getAllTodos() {
     return db.getAllItems();
+  }
+
+  public TodoItem addTodoItem(AddTodoDTO data) {
+    TodoItem item = createTodoItem(data);
+    db.addItem(item);
+    return item;
+  }
+
+  private TodoItem createTodoItem(AddTodoDTO data) {
+    String randomId = idGenerationService.generateRandomId();
+    return new TodoItem(randomId, data.getDescription(), TodoStatus.OPEN);
   }
 }
--- backend/src/test/java/de/neuefische/todoapp/controller/TodoControllerTest.java
+++ backend/src/test/java/de/neuefische/todoapp/controller/TodoControllerTest.java
@@ -3,11 +3,15 @@ package de.neuefische.todoapp.controller;
 import de.neuefische.todoapp.db.TodoDb;
 import de.neuefische.todoapp.model.TodoItem;
 import de.neuefische.todoapp.model.TodoStatus;
+import de.neuefische.todoapp.model.dto.AddTodoDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -25,16 +29,19 @@ class TodoControllerTest {
   @Autowired
   public TodoDb db;
 
+  @BeforeEach
+  public void resetDb(){
+    db.clearDb();
+  }
+
   @Test
   public void getTodoShouldReturnAllTodoItemsFromDb() {
     //GIVEN
     db.addItem(new TodoItem("1", "first todo", TodoStatus.OPEN));
     db.addItem(new TodoItem("2", "done todo", TodoStatus.DONE));
 
-    String serverUrl = "http://localhost:" + port + "/api/todo";
-
     //WHEN
-    ResponseEntity<TodoItem[]> response = restTemplate.getForEntity(serverUrl, TodoItem[].class);
+    ResponseEntity<TodoItem[]> response = restTemplate.getForEntity(getTodoApiUrl(), TodoItem[].class);
 
     //THEN
     assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -44,4 +51,26 @@ class TodoControllerTest {
     assertEquals(new TodoItem("2", "done todo", TodoStatus.DONE), items[1]);
   }
 
+  @Test
+  public void putTodoItemShouldAddATodoItemToDb() {
+    //GIVEN
+    HttpEntity<AddTodoDTO> entity = new HttpEntity<>(new AddTodoDTO("some todo"));
+
+    //WHEN
+    ResponseEntity<TodoItem> response = restTemplate.exchange(getTodoApiUrl(), HttpMethod.PUT, entity, TodoItem.class);
+
+    //THEN
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    TodoItem addedItem = response.getBody();
+    assertEquals("some todo", addedItem.getDescription());
+    assertEquals(TodoStatus.OPEN, addedItem.getStatus());
+    assertNotNull(addedItem.getId());
+
+    assertEquals(1, db.getAllItems().size());
+    assertTrue( db.getAllItems().contains(addedItem));
+  }
+
+  private String getTodoApiUrl() {
+    return "http://localhost:" + port + "/api/todo";
+  }
 }
